f9a2064609bb2a4def071d56e9e0ab5dad9ba352
sunghye-on <opei71@naver.com>
2020-06-13 00:14:12 +0900

    dropzone을 이용한 파일입력 폼 만들기

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,9 +1,20 @@
 import React, { useState } from "react";
 // antd라는 것을 이용해서 웹페이지를 쉽게 구성 머테리얼/부트스트랩과 같다
 import { Typography, Button, Form, Input, Descriptions } from "antd";
+import { FileUpload } from "../../utils/FileUpload";
 
 const { Title } = Typography;
 const { TextArea } = Input;
+
+const Continents = [
+  { key: 1, value: "아프리카" },
+  { key: 2, value: "유럽" },
+  { key: 3, value: "아시아" },
+  { key: 4, value: "남미" },
+  { key: 5, value: "북미" },
+  { key: 6, value: "호주" },
+  { key: 7, value: "태평양" },
+];
 
 function UploadProductPage() {
   const [Title, setTitle] = useState("");
@@ -24,13 +35,17 @@ function UploadProductPage() {
     setPrice(event.currentTarget.value);
   };
 
+  const ContinentChange = (event) => {
+    setContinent(event.currentTarget.value);
+  };
+
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}></div>
       {/* <Title level={2}>여행 샘플 업로드</Title> */}
       <Form>
         {/* DropZone */}
-
+        <FileUpload />
         <br />
         <br />
 
@@ -49,8 +64,12 @@ function UploadProductPage() {
         <br />
         <br />
 
-        <select>
-          <option></option>
+        <select onChange={ContinentChange} value={Continent}>
+          {Continents.map((obj) => (
+            <option key={obj.key} value={obj.key}>
+              {obj.value}
+            </option>
+          ))}
         </select>
         <br />
         <br />
