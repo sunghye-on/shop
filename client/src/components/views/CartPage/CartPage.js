db0ef9f22ae194afe1dc7497d8d06ef509d76f9d
sunghye-on <opei71@naver.com>
2020-07-09 14:40:07 +0900

    dispatch이후 온 정보를 이용해서 총합 구하기

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -5,28 +5,43 @@ import UserCartBlock from "./Section/UserCartBlock";
 function CartPage(props) {
   const dispatch = useDispatch();
   const [Cart, setCart] = useState();
+  const [Total, setTotal] = useState(0);
   useEffect(() => {
     const userData = props.user.userData;
     let cartData = [];
-
     // 유저안에 카트에 상품이 있는지 확인
     if (userData && userData.cart) {
       if (userData.cart.length > 0) {
         userData.cart.forEach((element) => {
           cartData.push(element.id);
         });
-        dispatch(getCartItems(cartData, userData.cart));
+        dispatch(getCartItems(cartData, userData.cart)).then((response) => {
+          gettotal(response.payload);
+        });
       }
       setCart(userData.cart);
     }
   }, [props.user.userData]);
-  console.log(props.user.cartDetail);
+  // const userCart = props.user.cartDetail;
+  const gettotal = (userCart) => {
+    let total = 0;
+    console.log(userCart);
+    for (const i in userCart) {
+      total += parseInt(userCart[i].price) * parseInt(userCart[i].quantity);
+      console.log(userCart[i].price, userCart[i].quantity);
+    }
+    setTotal(total);
+    return total;
+  };
+  // console.log(props.user.cartDetail);
   return (
     <div style={{ width: "85%", margin: "3rem auto" }}>
       <h1>장바구니</h1>
       <div>
         <UserCartBlock products={props.user.cartDetail} />
       </div>
+
+      <div style={{ marginTop: "3rem" }}>합계 {Total}원</div>
     </div>
   );
 }
